d921a2c45b95869e32c5f3e5675788c34d3cdf2c
Andon Gorchov <thunderman913@students.softuni.bg>
2021-09-04 23:49:15 +0300

    Made small changes in react structure

--- FriendyFy/ClientApp/src/App.js
+++ FriendyFy/ClientApp/src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
+import { Layout } from './components/Layout/Layout';
+import { Home } from './components/Home/Home';
+import { FetchData } from './components/FetchData/FetchData';
+import { Counter } from './components/Counter/Counter';
 import AuthorizeRoute from './components/api-authorization/AuthorizeRoute';
 import ApiAuthorizationRoutes from './components/api-authorization/ApiAuthorizationRoutes';
 import { ApplicationPaths } from './components/api-authorization/ApiAuthorizationConstants';
