4e2fdf9b465a7fa8288d0c90d89b22b1a8f44920
thunder913 <andon_00@abv.bg>
2021-11-24 19:31:58 +0200

    added some css stylings to the event page

--- FriendyFy/ClientApp/src/components/Event/Event.js
+++ FriendyFy/ClientApp/src/components/Event/Event.js
@@ -4,6 +4,7 @@ import EventOneImage from "./EventImages/EventOneImage";
 
 const Event = () => {
     const images = ['https://cdn.pixabay.com/photo/2017/07/21/23/57/concert-2527495__480.jpg']
+    const mainImage = 'https://media.istockphoto.com/photos/nicelooking-attractive-gorgeous-glamorous-elegant-stylish-cheerful-picture-id1165055006?k=20&m=1165055006&s=612x612&w=0&h=OD4-_BceL_R2eaaBzDQrXNIyydwYXOJX-m-0z12z17s=';
     return(
         <article className="event-page">
             <section className="event-page-top">
@@ -12,13 +13,23 @@ const Event = () => {
                 </div>
                 <div className="middle">
                     <div className="image">
-                        <img src={images[0]} alt="" />
+                        <img className="main-image" src={mainImage} alt="" />
+                        <div className="bottom-going">
+                        <h2>GOING:</h2>
+                        <div className="going-images">
+                            <img className="going-image" src="https://friendyfy.blob.core.windows.net/pictures/ba8b368f-e711-44dd-b611-41cdbf36fb3c.jpeg" alt="" />
+                            <img className="going-image" src="https://friendyfy.blob.core.windows.net/pictures/ba8b368f-e711-44dd-b611-41cdbf36fb3c.jpeg" alt="" />
+                            <img className="going-image" src="https://friendyfy.blob.core.windows.net/pictures/ba8b368f-e711-44dd-b611-41cdbf36fb3c.jpeg" alt="" />
+                            <img className="going-image" src="https://friendyfy.blob.core.windows.net/pictures/ba8b368f-e711-44dd-b611-41cdbf36fb3c.jpeg" alt="" />
+                        </div>
+                        </div>
                     </div>
                 </div>
                 <div className="location-time">
                     Location is here!!!!!!
                 </div>
             </section>
+            <h1>test</h1>
         </article>
     )
 }
