a59df4c660f9e307ddcf3acb90b2206c72b1df0b
thunder913 <andon_00@abv.bg>
2023-02-09 10:46:25 +0200

    Remove unused variables and mapping project

--- FriendyFy.Common/GlobalConstants.cs
+++ FriendyFy.Common/GlobalConstants.cs
@@ -1,15 +1,9 @@
-﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
-
-namespace FriendyFy.Common;
+﻿namespace FriendyFy.Common;
 
 public class GlobalConstants
 {
     public const string Email = "friendyfy@abv.bg";
     public const string BlobPictures = "pictures";
-    public const string BlobProfilePictures = "profilepictures";
-    public const string BlobCoverPictures = "coverpictures";
     public const string Issuer = "FriendyFy";
     public const string Audience = "User";
-    public const string AuthSchemes =
-        "Identity.Application" + "," + JwtBearerDefaults.AuthenticationScheme;
 }
